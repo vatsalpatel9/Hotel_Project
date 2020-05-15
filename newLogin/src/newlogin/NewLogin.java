772ed0edb861ac6544c4391cbcb19245f69e060c
“vatsalpatel9” <44619923+vatsalpatel9@users.noreply.github.com>
2020-05-15 18:23:29 -0500

    testing

---
+++ newLogin/src/newlogin/NewLogin.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newlogin;
+
+import newlogin.app.LoginForm;
+
+
+/**
+ *
+ * @author vatsalpatel
+ */
+public class NewLogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginForm loging = new LoginForm();
+    }
+    
+}
