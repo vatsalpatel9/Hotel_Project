0f48ee38342df1e6e236ee1dafb18c4c6f44025d
“vatsalpatel9” <44619923+vatsalpatel9@users.noreply.github.com>
2020-05-15 18:21:45 -0500

    management app

---
+++ loginForm/src/loginform/guestDatabaseConn.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginform;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author vatsalpatel
+ */
+public class guestDatabaseConn {
+    Connection conn = null;
+    public static Connection dbConnection(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:./GuestData.sqlite");
+           // JOptionPane.showMessageDialog(null, "ConnectionSuccessful!");
+            return conn;
+            
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
