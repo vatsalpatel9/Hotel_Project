575b15aa889accb8183cb0cba9e5ce1fe7374040
“vatsalpatel9” <44619923+vatsalpatel9@users.noreply.github.com>
2020-05-17 16:39:01 -0500

    Added Layouts

--- HotelProject/src/hotelproject/app/mainApp.java
+++ HotelProject/src/hotelproject/app/mainApp.java
@@ -7,9 +7,17 @@ package hotelproject.app;
 
 import java.sql.Connection;
 import hotelproject.db.guestDatabaseConn;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.WindowConstants;
@@ -27,7 +35,21 @@ public class mainApp extends JFrame{
         initComponents();
     }
     
+    private JButton homeBtn;
+    private JButton checkInBtn;
+    private JButton checkOutBtn;
+    
     private void initComponents(){
+        
+        homeBtn = new JButton("Home");
+        checkInBtn = new JButton("CheckIn");
+        checkOutBtn = new JButton("Checkout");
+        
+        //buttons
+        Dimension btnDim = new Dimension(190,50);
+        homeBtn.setPreferredSize(btnDim);
+        checkInBtn.setPreferredSize(btnDim);
+        checkOutBtn.setPreferredSize(btnDim);
         
         //Frame setUp
         Dimension frameDim = new Dimension(800, 600);
@@ -36,8 +58,44 @@ public class mainApp extends JFrame{
         setPreferredSize(frameDim);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         
+        //Container Panel
+        JPanel containerPanel = new JPanel();
+        containerPanel.setLayout(new BorderLayout());
+        
+        //Button Panel
+        JPanel btnPanel = new JPanel();
+        btnPanel.setBackground(Color.RED);
+        //btnPanel.setPreferredSize(new Dimension(200, 600));
+       // btnPanel.setMinimumSize(new Dimension(200, 600));
+        btnPanel.setSize(200,600);
+        btnPanel.setLayout(new GridBagLayout());
+        btnPanel.add(homeBtn, getLabelConstraints(0,0, GridBagConstraints.LINE_START));
+        btnPanel.add(checkInBtn, getLabelConstraints(0,1, GridBagConstraints.LINE_START));
+        btnPanel.add(checkOutBtn, getLabelConstraints(0,2, GridBagConstraints.LINE_START));
+        
+        
+        //Main Display Panel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBackground(Color.BLUE);
+        
+        //add Panels
+        containerPanel.add(btnPanel, BorderLayout.WEST);
+        containerPanel.add(mainPanel);
+        getContentPane().add(containerPanel);
+        
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+    
+    private GridBagConstraints getLabelConstraints(int x, int y, int anchor) {
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.VERTICAL;
+        c.insets = new Insets(5,5,5,5);
+        c.weightx = 0.5;
+        c.gridx = x;
+        c.gridy = y;
+        c.anchor = anchor;
+        return c;
     }
 }
