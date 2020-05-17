42b14aacce06c267b6a8f401b51ad304e77cd7ec
“vatsalpatel9” <44619923+vatsalpatel9@users.noreply.github.com>
2020-05-16 23:05:21 -0500

    Added Icon PlaceHolder

--- HotelProject/src/hotelproject/appLogin.java
+++ HotelProject/src/hotelproject/appLogin.java
@@ -1,28 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Login Page for Entry Into Management Software
+ * 
+ * @author vatsalpatel
  */
+
 package hotelproject;
 
 import java.sql.Connection;
 import hotelproject.db.loginDatabaseConn;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.GroupLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
-/**
- *
- * @author vatsalpatel
- */
 public class appLogin extends JFrame{
     Connection conn;
     
@@ -32,19 +32,18 @@ public class appLogin extends JFrame{
     }
     
     //components variable
-    private JFrame      loginFrame;
-    private GroupLayout loginFrameLayout;
     private JTextField  userNameField;
     private JTextField  passwordField;
     private JButton     loginBtn;
+    private JLabel      iconPlaceHolder;
     
     //components
     private void initComponents(){
         //objects
-        loginFrame = new JFrame();
         userNameField = new JTextField();
         passwordField = new JTextField();
         loginBtn = new JButton();
+        iconPlaceHolder = new JLabel("B", SwingConstants.CENTER);
         
         //Frame settings
         Dimension frameDim = new Dimension(350, 600);
@@ -63,6 +62,12 @@ public class appLogin extends JFrame{
         passwordField.setForeground(new java.awt.Color(153, 153, 153));
         passwordField.setPreferredSize(new Dimension(200, 50));
         
+        //LabelIcon Design
+        iconPlaceHolder.setBackground(Color.red);
+        iconPlaceHolder.setPreferredSize(new Dimension(100,100));
+        iconPlaceHolder.setBorder(BorderFactory.createLineBorder(Color.yellow));
+        iconPlaceHolder.setFont(new Font("Arial Rounded MT Bold", 0, 50));
+        iconPlaceHolder.setForeground(new Color(29, 171, 67));
         
         //Buttons Design
         loginBtn.setText("Go");
@@ -72,9 +77,10 @@ public class appLogin extends JFrame{
         JPanel loginPanel = new JPanel();
         loginPanel.setBackground(new Color(186, 215, 227));
         loginPanel.setLayout(new GridBagLayout());
-        loginPanel.add(userNameField, getConstraints(0,0, GridBagConstraints.PAGE_START));
-        loginPanel.add(passwordField, getConstraints(0,1, GridBagConstraints.PAGE_START));
-        loginPanel.add(loginBtn, getConstraints(0,2, GridBagConstraints.PAGE_START));
+        loginPanel.add(iconPlaceHolder, getLabelConstraints(0, 0, GridBagConstraints.PAGE_START));
+        loginPanel.add(userNameField, getConstraints(0,1, GridBagConstraints.CENTER));
+        loginPanel.add(passwordField, getConstraints(0,2, GridBagConstraints.CENTER));
+        loginPanel.add(loginBtn, getConstraints(0,3, GridBagConstraints.CENTER));
         
         getContentPane().add(loginPanel);
        
@@ -82,6 +88,17 @@ public class appLogin extends JFrame{
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+    
+    private GridBagConstraints getLabelConstraints(int x, int y, int anchor) {
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.VERTICAL;
+        c.insets = new Insets(0,20,20,20);
+        c.weightx = 0.5;
+        c.gridx = x;
+        c.gridy = y;
+        c.anchor = anchor;
+        return c;
     }
     
     private GridBagConstraints getConstraints(int x, int y, int anchor) {
