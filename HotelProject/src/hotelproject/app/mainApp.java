bb57e0a1669f04c8b5fa64326047102f07166192
“vatsalpatel9” <44619923+vatsalpatel9@users.noreply.github.com>
2020-05-19 13:49:08 -0500

    Check-In Panel Layout Ready

--- HotelProject/src/hotelproject/app/mainApp.java
+++ HotelProject/src/hotelproject/app/mainApp.java
@@ -14,9 +14,11 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -25,6 +27,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.WindowConstants;
@@ -50,6 +54,19 @@ public class mainApp extends JFrame{
     private JPanel      checkOutPanel;
     private JScrollPane tableScrollPane;
     private JTable      guestListTable;
+    private JLabel      fNameLabel;
+    private JLabel      lNameLabel;
+    private JLabel      addressLabel;
+    private JLabel      cityLabel;
+    private JLabel      stateLabel;
+    private JLabel      zipLabel;
+    private JTextField  fNameField;
+    private JTextField  lNameField;
+    private JTextField  addressField;
+    private JTextField  cityField;
+    private JTextField  stateField;
+    private JTextField  zipField;
+    private JButton     continueBtn;
     
     private void initComponents(){
         
@@ -69,7 +86,7 @@ public class mainApp extends JFrame{
         checkOutBtn.setName("checkOutBtn");
         
         //Frame setUp
-        Dimension frameDim = new Dimension(800, 600);
+        Dimension frameDim = new Dimension(1000, 600);
         setName("MainFrame");
         setMinimumSize(frameDim);
         setPreferredSize(frameDim);
@@ -93,7 +110,7 @@ public class mainApp extends JFrame{
         
         //Main Display guestTableScrol
         JLayeredPane mainPanel = new JLayeredPane();
-      //  mainPanel.setBackground(bgColor);
+        //  mainPanel.setBackground(bgColor);
         mainPanel.setPreferredSize(new Dimension(600, 600));
         mainPanel.setLayout(new CardLayout());
         
@@ -116,12 +133,97 @@ public class mainApp extends JFrame{
         guestListTable.setRowSelectionAllowed(false);
         tableScrollPane.setViewportView(guestListTable);
         
+        //checkinPanel
+        //checkInPanel.setLayout(new GridLayout(3,6));
+        fNameLabel = new JLabel("First Name");
+        lNameLabel = new JLabel("Last Name");
+        addressLabel = new JLabel("Address");
+        cityLabel = new JLabel("City");
+        stateLabel = new JLabel("State");
+        zipLabel = new JLabel("Zip Code");
+        
+        fNameField = new JTextField();
+        lNameField = new JTextField();
+        addressField = new JTextField();
+        cityField = new JTextField();
+        stateField = new JTextField();
+        zipField = new JTextField();
+        continueBtn = new JButton("Continue");
+        
+        continueBtn.setPreferredSize(new Dimension(97,32));
+        
+        Dimension fieldDim = new Dimension(75,15);
+        fNameField.setPreferredSize(fieldDim);
+        
+        GroupLayout checkInPanelLayout = new GroupLayout(checkInPanel);
+        checkInPanel.setLayout(checkInPanelLayout);
+        checkInPanelLayout.setHorizontalGroup(
+                checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addGroup(checkInPanelLayout.createSequentialGroup()
+                        .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addGroup(checkInPanelLayout.createSequentialGroup()
+                                .addGap(51,51,51)
+                                .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                    .addComponent(fNameLabel, GroupLayout.PREFERRED_SIZE, 107,GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(addressLabel, GroupLayout.PREFERRED_SIZE, 107, GroupLayout.PREFERRED_SIZE))
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addGroup(checkInPanelLayout.createSequentialGroup()
+                                    .addComponent(fNameField, GroupLayout.PREFERRED_SIZE, 208, GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(lNameLabel, GroupLayout.PREFERRED_SIZE, 107, GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(lNameField, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE))
+                                .addComponent(continueBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(addressField)))
+                            .addGroup(checkInPanelLayout.createSequentialGroup()
+                                .addGap(49,49,49)
+                                .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                    .addGroup(checkInPanelLayout.createSequentialGroup()
+                                        .addComponent(cityLabel, GroupLayout.PREFERRED_SIZE, 107, GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(cityField, GroupLayout.PREFERRED_SIZE, 228, GroupLayout.PREFERRED_SIZE)
+                                        .addGap(30, 30, 30)
+                                        .addComponent(stateLabel)
+                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(stateField, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+                                        .addGap(47, 47, 47)
+                                        .addComponent(zipLabel)
+                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(zipField, GroupLayout.DEFAULT_SIZE, 126, Short.MAX_VALUE)))))
+                        .addGap(38,38,38))
+        );
+        checkInPanelLayout.setVerticalGroup(
+            checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(checkInPanelLayout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(fNameField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lNameField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(fNameLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lNameLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(addressLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(addressField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(cityField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(stateLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(stateField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(zipLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(zipField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cityLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 165, Short.MAX_VALUE)
+                .addComponent(continueBtn, GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
+                .addGap(84, 84, 84))
+        );     
+        
         checkInPanel.setBackground(bgColor);
         checkInPanel.setVisible(false);
-        
         checkOutPanel.setVisible(false);
 
-        
         //addPanels to layered Pane
         mainPanel.add(tableScrollPane, "card1");
         mainPanel.add(checkInPanel, "card2");
@@ -158,7 +260,7 @@ public class mainApp extends JFrame{
         c.anchor = anchor;
         return c;
     }
-    
+
     private void popTable(JTable table) {
         table.getTableHeader().setFont(new Font("SansSerif", Font.ITALIC, 18));
         try {
@@ -195,4 +297,5 @@ public class mainApp extends JFrame{
                 break;
         }
     }
+    
 }
