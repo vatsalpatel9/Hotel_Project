7390a86a541e0fd749dea5d6a2ca0cf71b797e23
“vatsalpatel9” <44619923+vatsalpatel9@users.noreply.github.com>
2020-05-22 21:19:53 -0500

    cleanup

--- HotelProject/src/hotelproject/HotelProject.java
+++ HotelProject/src/hotelproject/HotelProject.java
@@ -17,7 +17,7 @@ public class HotelProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new mainApp();
+        new appLogin();
     }
     
 }
