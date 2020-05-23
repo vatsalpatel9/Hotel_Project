c2d79f56ddcabb79864141017c287aa416794635
“vatsalpatel9” <44619923+vatsalpatel9@users.noreply.github.com>
2020-05-23 16:45:30 -0500

    ReOrganize

--- HotelProject/src/hotelproject/app/mainApp.java
+++ HotelProject/src/hotelproject/app/mainApp.java
@@ -21,11 +21,8 @@ import java.awt.event.MouseEvent;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -44,14 +41,15 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.WindowConstants;
 import javax.swing.table.TableCellRenderer;
-import javax.swing.text.MaskFormatter;
 import net.proteanit.sql.DbUtils;
 
 public class mainApp extends JFrame{
     Connection conn;
+ 
     private static final DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
     public mainApp(){
         conn = guestDatabaseConn.dbConnection();
+        
         try{
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         }catch(ClassNotFoundException | InstantiationException |
@@ -61,23 +59,15 @@ public class mainApp extends JFrame{
         initComponents();
     }
     
+    private checkInPanelApp checkinpanel;
     private JButton     homeBtn;
     private JButton     checkInBtn;
     private JButton     checkOutBtn;
-    private JPanel      checkInPanel;
     private JPanel      guestDetailPanel;
     private JScrollPane checkOutPanel;
     private JScrollPane tableScrollPane;
     private JTable      guestListTable;
     private JTable      checkOutTable;
-    private JLabel      fNameLabel;
-    private JLabel      lNameLabel;
-    private JLabel      addressLabel;
-    private JLabel      cityLabel;
-    private JLabel      stateLabel;
-    private JLabel      zipLabel;
-    private JLabel      numOfNightsLabel;
-    private JLabel      roomRateLabel;
     private JLabel      showGuestIdLabel;
     private JLabel      showFNameLabel;
     private JLabel      showLNameLabel;
@@ -88,13 +78,6 @@ public class mainApp extends JFrame{
     private JLabel      showRateLabel;
     private JLabel      showAirDateLabel;
     private JLabel      showDepDateLabel;
-    private JLabel      roomNumLabel;
-    private JTextField  fNameField;
-    private JTextField  lNameField;
-    private JTextField  addressField;
-    private JTextField  cityField;
-    private JTextField  stateField;
-    private JTextField  zipField;
     private JTextField  showGuestIdField;
     private JTextField  showFNameField;
     private JTextField  showLNameField;
@@ -105,11 +88,8 @@ public class mainApp extends JFrame{
     private JTextField  showRateField;
     private JTextField  showAirDateField;
     private JTextField  showDepDateField;
-    private JFormattedTextField  roomRateField;
-    private JButton     continueBtn;
-    private JSpinner    numOfNights;
     private JComboBox   roomNum;
-    
+
     private void initComponents(){
         
         homeBtn = new JButton("Home");
@@ -152,7 +132,7 @@ public class mainApp extends JFrame{
         
         //Main Display guestTableScrol
         JLayeredPane mainPanel = new JLayeredPane();
-        //  mainPanel.setBackground(bgColor);
+        mainPanel.setBackground(bgColor);
         mainPanel.setPreferredSize(new Dimension(600, 600));
         mainPanel.setLayout(new CardLayout());
         
@@ -162,7 +142,7 @@ public class mainApp extends JFrame{
         
         //Panel for Layered Pane
         tableScrollPane = new JScrollPane();
-        checkInPanel = new JPanel();
+
         checkOutPanel = new JScrollPane();
         guestDetailPanel = new JPanel();
         
@@ -176,125 +156,8 @@ public class mainApp extends JFrame{
         guestListTable.setRowSelectionAllowed(false);
         tableScrollPane.setViewportView(guestListTable);
         
-        //checkinPanel
-        //checkInPanel.setLayout(new GridLayout(3,6));
-        fNameLabel = new JLabel("First Name");
-        lNameLabel = new JLabel("Last Name");
-        addressLabel = new JLabel("Address");
-        cityLabel = new JLabel("City");
-        stateLabel = new JLabel("State");
-        zipLabel = new JLabel("Zip Code");
-        numOfNightsLabel = new JLabel("Nights");
-        roomRateLabel = new JLabel("Rate");
-        roomNumLabel = new JLabel("Room");
-        
-        fNameField = new JTextField();
-        lNameField = new JTextField();
-        addressField = new JTextField();
-        cityField = new JTextField();
-        stateField = new JTextField();
-        zipField = new JTextField();
-        continueBtn = new JButton("Continue");
-        numOfNights = new JSpinner();
-        roomNum = new JComboBox();
-        roomRateField = new JFormattedTextField();
-        MaskFormatter moneyMask = null;
-        try {
-            moneyMask = new MaskFormatter("##.##");
-        } catch (ParseException ex) {
-            Logger.getLogger(mainApp.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        moneyMask.install(roomRateField);
-        displayRoom();
-        
-        GroupLayout checkInPanelLayout = new GroupLayout(checkInPanel);
-        checkInPanel.setLayout(checkInPanelLayout);
-        checkInPanelLayout.setHorizontalGroup(
-                checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                    .addGroup(checkInPanelLayout.createSequentialGroup()
-                        .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                            .addGroup(checkInPanelLayout.createSequentialGroup()
-                                .addGap(51,51,51)
-                                .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                    .addComponent(fNameLabel, GroupLayout.PREFERRED_SIZE, 107,GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(addressLabel, GroupLayout.PREFERRED_SIZE, 107, GroupLayout.PREFERRED_SIZE))
-                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                            .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                .addGroup(checkInPanelLayout.createSequentialGroup()
-                                    .addComponent(fNameField, GroupLayout.PREFERRED_SIZE, 208, GroupLayout.PREFERRED_SIZE)
-                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(lNameLabel, GroupLayout.PREFERRED_SIZE, 107, GroupLayout.PREFERRED_SIZE)
-                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(lNameField, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE))
-                                .addComponent(continueBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(addressField)))
-                            .addGroup(checkInPanelLayout.createSequentialGroup()
-                                .addGap(49,49,49)
-                                .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                     .addGroup(checkInPanelLayout.createSequentialGroup()
-                                        .addComponent(roomRateLabel, GroupLayout.PREFERRED_SIZE, 107, GroupLayout.PREFERRED_SIZE)
-                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(roomRateField, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
-                                        .addGap(184,184,184)
-                                        .addComponent(numOfNightsLabel)
-                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(numOfNights, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
-                                        .addGap(45,45,45)
-                                        .addComponent(roomNumLabel)
-                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(roomNum, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE))
-                                     .addGroup(checkInPanelLayout.createSequentialGroup()
-                                        .addComponent(cityLabel, GroupLayout.PREFERRED_SIZE, 107, GroupLayout.PREFERRED_SIZE)
-                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(cityField, GroupLayout.PREFERRED_SIZE, 228, GroupLayout.PREFERRED_SIZE)
-                                        .addGap(30, 30, 30)
-                                        .addComponent(stateLabel)
-                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(stateField, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
-                                        .addGap(47, 47, 47)
-                                        .addComponent(zipLabel)
-                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(zipField, GroupLayout.DEFAULT_SIZE, 126, Short.MAX_VALUE)))))
-                        .addGap(38,38,38))
-        );
-        checkInPanelLayout.setVerticalGroup(
-            checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(checkInPanelLayout.createSequentialGroup()
-                .addGap(28, 28, 28)
-                .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(fNameField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lNameField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(fNameLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lNameLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(addressLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(addressField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(cityField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(stateLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(stateField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(zipLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(zipField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(cityLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(checkInPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(roomRateLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(roomRateField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(numOfNightsLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(numOfNights, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(roomNumLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(roomNum, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                    .addGroup(checkInPanelLayout.createSequentialGroup()
-                        .addGap(5, 5, 5)))
-                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 165, Short.MAX_VALUE)
-                .addComponent(continueBtn, GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
-                .addGap(84, 84, 84))
-        );       
-        checkInPanel.setBackground(bgColor);
-        
+        checkinpanel = new checkInPanelApp();
+    
         //checkOutPanel
         checkOutTable = new JTable(){
             public Component prepareRenderer(TableCellRenderer renderer, int row, int column)
@@ -451,11 +314,11 @@ public class mainApp extends JFrame{
         //END OF GUEST DETAIL PANEL
 
         //addPanels to layered Pane
-        checkInPanel.setVisible(false);
+        checkinpanel.setVisible(false);
         checkOutPanel.setVisible(false);
         guestDetailPanel.setVisible(false);
         mainPanel.add(tableScrollPane, "card1");
-        mainPanel.add(checkInPanel, "card2");
+        mainPanel.add(checkinpanel, "card2");
         mainPanel.add(checkOutPanel, "card3");
         mainPanel.add(guestDetailPanel, "card4");
         
@@ -468,9 +331,6 @@ public class mainApp extends JFrame{
         });
         checkOutBtn.addActionListener((ActionListner) -> {
             switchPanel(checkOutBtn);
-        });
-        continueBtn.addActionListener((ActionListner) -> {
-            checkInGuest();
         });
         
         //add Panels to main frame
@@ -524,7 +384,7 @@ public class mainApp extends JFrame{
         String btnName = btn.getName();
         switch(btnName){
             case "homeBtn":
-                checkInPanel.setVisible(false);
+                checkinpanel.setVisible(false);
                 checkOutPanel.setVisible(false);
                 popRoomTable(guestListTable);
                 tableScrollPane.setVisible(true);
@@ -532,11 +392,11 @@ public class mainApp extends JFrame{
             case "checkInBtn":
                 tableScrollPane.setVisible(false);
                 checkOutPanel.setVisible(false);
-                checkInPanel.setVisible(true);
+                checkinpanel.setVisible(true);
                 break;
             case "checkOutBtn":
                 tableScrollPane.setVisible(false);
-                checkInPanel.setVisible(false);
+                checkinpanel.setVisible(false);
                 popTable(checkOutTable);
                 checkOutPanel.setVisible(true);
                 break;
@@ -544,109 +404,17 @@ public class mainApp extends JFrame{
                 break;
         }
     }
-        
-    private void checkInGuest(){
-        try{
-            String query = "INSERT INTO 'guestList' (FirstName, LastName, Address, City, State, ZipCode, Rate, ArrivalDate, DepartureDate) Values(?,?,?,?,?,?,?,?,?)";
-            PreparedStatement pst;
-            pst = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
-            pst.setString(1, fNameField.getText());
-            pst.setString(2, lNameField.getText());
-            pst.setString(3, addressField.getText());
-            pst.setString(4, cityField.getText());
-            pst.setString(5, stateField.getText());
-            pst.setString(6, zipField.getText());
-            pst.setString(7, roomRateField.getText());
-            pst.setString(8, getCurrentDate());
-            pst.setString(9, setCheckOutDate((int) numOfNights.getValue()));
-            
-            int n1 = pst.executeUpdate();
-            ResultSet rs = pst.getGeneratedKeys();
-            int GuestID = 0;
-            if(rs.next()){
-                GuestID = rs.getInt(1);
-            }
-            rs.close();
-            
-            String selRoom = (String) roomNum.getSelectedItem();
-            int room = Integer.parseInt(selRoom.substring(0, 2));
-            
-            String roomQuery = "UPDATE 'roomView' SET RoomRate = ?, GuestID = ?, FirstName = ?, LastName = ?, Arrival = ?, Departure = ?, RoomStatus = 'DIRTY' WHERE RoomNum = '"+room+"'";
-            PreparedStatement pstRoom = conn.prepareStatement(roomQuery);
-            pstRoom.setString(1, roomRateField.getText());
-            pstRoom.setInt(2, GuestID);
-            pstRoom.setString(3, fNameField.getText());
-            pstRoom.setString(4, lNameField.getText());
-            pstRoom.setString(5, getCurrentDate());
-            pstRoom.setString(6, setCheckOutDate((int) numOfNights.getValue()));
-            
-            
-            int n2 = pstRoom.executeUpdate();
-            if(n1 > 0 && n2 > 0){
-                JOptionPane.showMessageDialog(null, "Success");
-                clearJTextFields(checkInPanel);
-                resetJSpinner(checkInPanel);
-            }
-            
-            pst.close();
-            pstRoom.close();
-            
-        }catch (Exception e){
-            JOptionPane.showMessageDialog(null, e);
-        }
-    }
-    
-    private void displayRoom(){
-        try{
-            String query = "SELECT RoomNum, RoomType, RoomStatus from roomView";
-            PreparedStatement pst = conn.prepareStatement(query);
-            ResultSet rs = pst.executeQuery();
-            
-            while(rs.next()){
-                String roomStatus = rs.getString("RoomStatus");
-                if(roomStatus.equals("CLEAN")){
-                    roomNum.addItem(rs.getInt("RoomNum") +" "+ rs.getString("RoomType"));
-                }
-            }
-        }catch (Exception e){
-            JOptionPane.showMessageDialog(null, e);
-        }
-    }
     
     private static String getCurrentDate(){
         Calendar cal = Calendar.getInstance();
         return sdf.format(cal.getTime());
     }
-    
-    private static String setCheckOutDate(int date){
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_YEAR, date);
-        return sdf.format(cal.getTime());
-    }
-    
-    private void clearJTextFields(JPanel panel){
-        for(Component control: panel.getComponents()){
-            if(control instanceof JTextField){
-                JTextField ctrl = (JTextField) control;
-                ctrl.setText(null);
-            }
-        }
-    }
-    
+
     private void setTextFieldProperty(JPanel panel){
         for(Component control: panel.getComponents()){
             if(control instanceof JTextField){
                 JTextField ctrl = (JTextField) control;
                 ctrl.setEditable(false);
-            }
-        }
-    }
-    
-    private void resetJSpinner(JPanel panel){
-        for(Component control: panel.getComponents()){
-            if(control instanceof JSpinner){
-                JSpinner ctrl = (JSpinner) control;
-                ctrl.setValue(0);
             }
         }
     }
